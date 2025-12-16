fb99bc003bc9161940e1073af12b372bb280f91e
SidharthSinghShrinet <tushar15463@gmail.com>
2025-12-16 23:24:49 +0530

    FInal Fix i.e. await connectDB()

--- app/(protected-root)/layout.js
+++ app/(protected-root)/layout.js
@@ -2,7 +2,9 @@ import { cookies } from "next/headers";
 import sessionCollection from "@/model/session.model";
 import jwt from "jsonwebtoken";
 import { redirect } from "next/navigation";
+import connectDB from "@/config/database";
 export default async function ProtectedRootLayout({ children }) {
+  await connectDB();
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
   if (!token) {
--- libs/auth.js
+++ libs/auth.js
@@ -1,6 +1,8 @@
+import connectDB from "@/config/database";
 import sessionCollection from "@/model/session.model";
 import jwt from "jsonwebtoken";
 export async function getAuthUser(token) {
+  await connectDB();
   const { sessionId } = jwt.decode(token);
   // console.log("Decoded Token:", sessionId);
   const { userId } = await sessionCollection.findOne({ sessionId });
